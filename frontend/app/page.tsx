82d8c4d881f647ba539de102b666cf5cbd108e5e
botgardo <edgardovalle2005@gmail.com>
2026-08-02 20:06:09 -0700

    Se agrego el menuSection y se modifico navMenu para mobile

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,14 @@
 import NavMenu from "./components/layout/navbar";
 import Hero from "./components/sections/hero";
 import Image from "next/image";
+import Menu from "./components/sections/menu";
 
 export default function Home() {
   return (
     <div>
       <NavMenu />
       <Hero />
-      <div id="menu" className="min-h-screen flex justify-center items-center">Menu</div>
+      <Menu/>
       <div id="about" className="min-h-screen flex justify-center items-center">Nosotros</div>
       <div id="sucursales" className="min-h-screen flex justify-center items-center">Sucursales</div>
       <div id="contact" className="min-h-screen flex justify-center items-center">Contacto</div>
